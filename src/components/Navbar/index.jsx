dbb3a8f7e945ebcb5ce0964e47c9c2b33a9d0169
ingucell <mathiaslawson70@gmail.com>
2023-04-24 11:25:27 +0000

    navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import logo from './central.png'
 
 function index() {
   return (
-    <div>index</div>
+   <>
+     <Box display='flex' justifyContent='space-between' padding='0.1rem' backgroundColor='white'>
+          <Box marginLeft='3rem'>
+            <img src={logo}/>
+          </Box>
+          {/* <Box>b</Box> */}
+     </Box>
+   </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './containers/App'
+import Navbar from './components/Navbar'
 import { CssBaseline } from '@mui/material';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ThemeProviderWrapper from './ThemeProviderWrapper.jsx';
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <CssBaseline />
           <div >
             <main className='content'>
+              <Navbar />
           <Router>
             <App />
           </Router>
