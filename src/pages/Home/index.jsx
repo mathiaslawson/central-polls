6843889bcb2e6fdfd01f3c70f56791d880244bf2
ingucell <mathiaslawson70@gmail.com>
2023-04-28 22:47:18 +0000

    Home Start

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,9 +4,14 @@ import {Box} from '@mui/material'
 function index() {
   return (
     <>
-       <Box>
-            Home
-
+       <Box padding='1rem'>
+            <Box height='50vh' display='flex' justifyContent='space-between'>
+              <Box>Details</Box>
+              <Box>iamge</Box>
+            </Box>
+            <Box height='50vh'>b</Box>
+            <Box height='50vh'>c</Box>
+            <Box height='50vh'>d</Box>
        </Box>    
     </>
   )
