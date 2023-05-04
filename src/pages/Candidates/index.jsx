2100f47d2f6525e5f60496343805ee7d036e846f
ingucell <mathiaslawson70@gmail.com>
2023-05-04 14:58:12 +0000

    candidate data from firebase

--- src/pages/Candidates/index.jsx
+++ src/pages/Candidates/index.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { store } from '../../store'
 import Logout from '../../actions/Logout'
+import Firebase from '../../services'
 
 function Candidates() {
 
- 
+  const [candidates, setCandidates] = useState([])
+
+  useEffect(()=>{
+    const firebase = new Firebase()
+    const collection = firebase.db.collection('candidates')
+    const  candidates = collection.onSnapshot((querySnapshot)=>{
+       const data = querySnapshot.docs.map((doc)=> doc.data());
+       setCandidates(data)
+       console.log(data)
+    })
+    return () => {
+       candidates()
+    }
+  }, [])
+
   return (
     <>
     <p>Welcome {store.getState().auth.user}</p>
@@ -14,6 +29,22 @@ function Candidates() {
        window.location.href='./home'
     }}
     >Logout</button>
+    <p>as</p>
+    
+    {/* Candidate INfo */}
+    <p>Candidate Information From fireStore</p>
+    <div>
+           {
+            candidates.map((member)=>( 
+              <div key={member.candidateName}>    
+               <p>{member.candidateName}</p>
+               <p>{member.candidatePosition}</p>
+               <p>{member.candidatePromises}</p>
+               <p>{member.candidateSolgan}</p>
+              </div>    
+            ))
+           }
+           </div>
     </>
   )
 }
