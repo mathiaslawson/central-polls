375f0aa33e86159ff4176aa50238c91aab016454
ingucell <mathiaslawson70@gmail.com>
2023-05-05 20:28:11 +0000

    canidates container

--- src/containers/CandidatesContainer/index.jsx
+++ src/containers/CandidatesContainer/index.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Candidates from '../../pages/Candidates'
+import Firebase from '../../services'
 
 function index() {
+
+    const [candidates, setCandidates] = useState([])
+
+    useEffect(()=>{
+        const firebase = new Firebase()
+        const collection = firebase.db.collection('candidates')
+        const  candidates = collection.onSnapshot((querySnapshot)=>{
+           const data = querySnapshot.docs.map((doc)=> doc.data());
+           setCandidates(data)
+           console.log(data)
+        })
+        return () => {
+           candidates()
+        }
+      }, [])
+
+
   return (
     <>
-      <Candidates />
+      <Candidates candidates={candidates}/>
     
     </>
   )
--- src/pages/Candidates/index.jsx
+++ src/pages/Candidates/index.jsx
@@ -1,35 +1,20 @@
 import React, {useEffect, useState} from 'react'
 import { store } from '../../store'
 import Logout from '../../actions/Logout'
-import Firebase from '../../services'
 
-function Candidates() {
+function Candidates({candidates}) {
 
-  const [candidates, setCandidates] = useState([])
-
-  useEffect(()=>{
-    const firebase = new Firebase()
-    const collection = firebase.db.collection('candidates')
-    const  candidates = collection.onSnapshot((querySnapshot)=>{
-       const data = querySnapshot.docs.map((doc)=> doc.data());
-       setCandidates(data)
-       console.log(data)
-    })
-    return () => {
-       candidates()
-    }
-  }, [])
-
+ 
   return (
     <>
-    <p>Welcome {store.getState().auth.user}</p>
+    {/* <p>Welcome {store.getState().auth.user}</p>
     <button
     onClick={()=>{
        store.dispatch(Logout(null))
        window.location.href='./home'
     }}
     >Logout</button>
-    <p>as</p>
+    <p>as</p> */}
     
     {/* Candidate INfo */}
     <p>Candidate Information From fireStore</p>
