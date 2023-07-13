ff68f35bc9fdc17e0858f473886d147c282e00df
ingucell <mathiaslawson70@gmail.com>
2023-07-13 12:25:15 +0000

    new user

--- src/data/users.js
+++ src/data/users.js
@@ -11,6 +11,9 @@ const users = [
    { 'id' : 'DPH/20/01/2041',
     'mail' : 'philemon.akotey@central.edu.gh'}
     ,
+   { 'id' : 'INT/22/01/2119',
+    'mail' : 'int22012119@central.edu.gh'}
+    ,
 ];
 
 export default users;
