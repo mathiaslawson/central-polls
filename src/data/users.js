d5c066c8bec54220e291c838f8421b86b5c920dd
Lizer2050 <lizerjinadu@gmai.com>
2024-07-17 23:54:47 +0000

    chore: Added new users

--- src/data/users.js
+++ src/data/users.js
@@ -1,19 +1,27 @@
 const users = [
    { 'id' : 'CSC/20/01/1847',
-    'mail' : 'mathias.lawson@central.edu.gh'}
+    'mail' : 'mathiaslawson@central.edu.gh'}
     ,
-   { 'id' : 'DPH/20/01/3092',
-    'mail' : 'matey.jonathan@central.edu.gh'}
+
+    { 'id' : 'CSC/20/01/2251',
+    'mail' : 'lizerjinadu@central.edu.gh'}
     ,
-   { 'id' : 'CSC/20/01/2709',
-    'mail' : 'ackney.gideon@central.edu.gh'}
+
+    { 'id' : 'CSC/20/01/1851',
+    'mail' : 'lillianhammond@central.edu.gh'}
     ,
-   { 'id' : 'DPH/20/01/2041',
-    'mail' : 'philemon.akotey@central.edu.gh'}
+
+    { 'id' : 'CSC/20/01/2709',
+    'mail' : 'gideonackney@central.edu.gh'}
     ,
-   { 'id' : 'INT/22/01/2119',
-    'mail' : 'int22012119@central.edu.gh'}
-    ,
+
+    { 'id' : 'CSC/20/01/2093',
+    'mail' : 'jeffersonjose@central.edu.gh'}
+    
+   
+    
+
+    
 ];
 
 export default users;
